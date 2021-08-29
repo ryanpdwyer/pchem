cf6a43f9051bfc6bd661e1b2549cdf801ef75524
Ryan Dwyer <ryanpdwyer@gmail.com>
2021-08-29 19:45:19 -0400

    Updated to add more little helpers along the way

--- apps/combineCSV.py
+++ apps/combineCSV.py
@@ -1,4 +1,5 @@
 
+from re import sub
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -51,10 +52,52 @@ def combine_spectra(dataframes, filenames, xcol, ycol, tol=1e-6):
         col_names.append(f"{ind}-{before_ext}")
     
     return pd.DataFrame(np.array(all_data).T, columns=col_names)
+
+
+def limit_x_values(combined_data, x_column, settings):
+    st.markdown("### Limit x Range")
+    x_data = combined_data[x_column].values
+    x_min_val = st.selectbox("Choose minimum x:", x_data, index=0 )
+    i_min = find(x_min_val, x_data)
+    x_max_val = st.selectbox("Choose maximum x:", x_data, index=len(x_data)-1 )
+    i_max = find(x_max_val, x_data)
+    combined_data = combined_data.iloc[i_min:i_max, :]
+    settings['x_min'] = x_min_val
+    settings['x_max'] = x_max_val
+    return combined_data, settings
+
+def normalize_data(combined_data, x_column, settings):
+    st.markdown("### Normalization options")
+    x_data = combined_data[x_column].values
+    processing_options = ['None', "Normalized", "Relative"]
+    processing = st.selectbox("Processing?", processing_options)
+    settings['processing'] = processing
+    if processing == 'Normalized':
+        normalize_wavelength = st.selectbox("Normalize data at: ", x_data)
+        settings['normalization_wavelength'] = normalize_wavelength
+    else:
+        settings.pop('normalization_wavelength', 0)
+
+    if processing == "Normalized":
+        norm_ind = find(normalize_wavelength, x_data)
+        y_data = combined_data.values[:, 1:]
+        combined_data.values[:, 1:] = y_data / y_data[norm_ind]
+                
+    if processing == "Relative":
+        # Should probably be tweaked a bit to be more convenient...
+        y_data = combined_data.values[:, 1:]
+        combined_data.values[:, 1:] = y_data / y_data.max(axis=0)
+    
+    return combined_data, settings
+
 def run():
     df = None
     cols = None
     x_column = y_column = None
+    combined_data = None
+    if 'ever_submitted' not in st.session_state:
+        st.session_state.ever_submitted = False
+    settings = {}
     st.markdown("""## Combine CSV files
 
 This helper will combine multiple CSV files (or Excel spreadsheets)
@@ -85,47 +128,45 @@ into a single Excel file for easy plotting and anaysis.
             
 
             submitted = st.form_submit_button()
-            if submitted:
-                combined_data = combine_spectra(data, filenames, x_column, y_column)
-                x_data = combined_data[x_column].values
-                x_min_val = st.selectbox("Choose minimum x:", x_data, index=0 )
-                i_min = find(x_min_val, x_data)
-                x_max_val = st.selectbox("Choose maximum x:", x_data, index=len(x_data)-1 )
-                i_max = find(x_max_val, x_data)
-                processing_options = ['None', "Normalized", "Relative"]
-                processing = st.selectbox("Processing?", processing_options)
-                combined_data = combined_data.iloc[i_min:i_max, :]
-                x_data = x_data[i_min: i_max]
-                normalize_wavelength = st.selectbox("Normalize data at: ", x_data)
 
-                if processing == "Normalized":
-                    norm_ind = find(normalize_wavelength, x_data)
-                    y_data = combined_data.values[:, 1:]
-                    combined_data.values[:, 1:] = y_data / y_data[norm_ind]
+        
+        st.session_state.ever_submitted = submitted | st.session_state.ever_submitted
+        if st.session_state.ever_submitted:
+            combined_data = combine_spectra(data, filenames, x_column, y_column)
+        
                 
-                if processing == "Relative":
-                    # Should probably be tweaked a bit to be more convenient...
-                    y_data = combined_data.values[:, 1:]
-                    combined_data.values[:, 1:] = y_data / y_data.max(axis=0)
 
-                fig, ax = plt.subplots()
-                for col, fname in zip(combined_data.values[:, 1:].T, filenames):
-                    ax.plot(x_data, col, label=str(fname[0])+"-"+fname[1])
                 
-                y_label_default = ""
-                if processing != 'None':
-                    y_label_default += processing+" "
-                y_label_default+=y_column
-                    
-                x_label = st.text_input("x-axis label: ", value=x_column)
-                y_label = st.text_input('y-axis label: ', value=y_label_default)
-                ax.set_xlabel(x_label)
-                ax.set_ylabel(y_label)
-                ax.legend()
-                st.pyplot(fig)
-                st.write(combined_data)
-                filename = st.text_input("Filename:", value="data")
-                write_excel(combined_data, filename)
+                
+        if combined_data is not None:
+            combined_data, settings = limit_x_values(combined_data, x_column, settings)
+            combined_data, settings = normalize_data(combined_data, x_column, settings)
+            x_data = combined_data[x_column].values
+            # Plotting
+            fig, ax = plt.subplots()
+            for col, fname in zip(combined_data.values[:, 1:].T, filenames):
+                ax.plot(x_data, col, label=str(fname[0])+"-"+fname[1])
+            
+
+            y_label_default = ""
+            if settings['processing'] != 'None':
+                y_label_default += settings['processing']+" "
+            y_label_default+=y_column
+
+
+            st.markdown("### Plotting options")    
+            x_label = st.text_input("x-axis label: ", value=x_column)
+            y_label = st.text_input('y-axis label: ', value=y_label_default)
+            ax.set_xlabel(x_label)
+            ax.set_ylabel(y_label)
+            ax.legend()
+            st.pyplot(fig)
+
+            # Saving
+            st.markdown("### Output options")
+            st.write(combined_data)
+            filename = st.text_input("Filename:", value="data")
+            write_excel(combined_data, filename)
 
 
 
