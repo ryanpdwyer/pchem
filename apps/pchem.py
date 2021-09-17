6d36f51175ac714008a11f1694131cfea4dadade
Ryan Dwyer <ryanpdwyer@gmail.com>
2021-09-16 23:28:00 -0400

    Fancy AI update

--- apps/pchem.py
+++ apps/pchem.py
@@ -4,11 +4,13 @@ import streamlit as st
 import combineCSV
 import combineCSVElectrochem
 import ace
+import openai_ex
 
 st.title("Physical Chemistry Helpers")
 
 apps = {"Combine CSV": combineCSV, 
-        "Combine CSV Electrochem": combineCSVElectrochem
+        "Combine CSV Electrochem": combineCSVElectrochem,
+        "AI Demo": openai_ex
 # 'ACE Editor': ace
 }
 
