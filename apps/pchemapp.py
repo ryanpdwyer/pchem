f1aa66c08fec9fbf8f54201f7f1acc7ee163eca0
Ryan Dwyer <ryanpdwyer@gmail.com>
2021-10-12 22:22:43 -0400

    Added to dropbox on main page

--- apps/pchemapp.py
+++ apps/pchemapp.py
@@ -4,16 +4,18 @@ import streamlit as st
 import combineCSV
 import combineCSVElectrochem
 import thermoFirstLaw
+import thermoCalorimeter
 import ace
 import openai_ex
 import solartronData
 
-st.title("Physical Chemistry Helpers")
+st.title("Physical Chemistry Tools")
 
 apps = {"Combine UV-Vis Data": combineCSV, 
         "Combine CSV Electrochem": combineCSVElectrochem,
         "Plot Solartron Data": solartronData,
         "1st Law of Thermodynamics": thermoFirstLaw,
+        "2nd Law Calorimeter": thermoCalorimeter,
         "Sympy Shell": ace,
         "AI Demo": openai_ex
 # 'ACE Editor': ace
