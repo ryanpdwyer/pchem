434695a7ad8e7b99795fdba7b9d4eb8e5965fa79
Ryan Dwyer <ryanpdwyer@gmail.com>
2022-10-28 16:19:12 -0400

    Added keys

--- pchem/__init__.py
+++ pchem/__init__.py
@@ -73,13 +73,55 @@ class Solve:
 
 solve = Solve() # Instantiate the class...
 
-def getprop(gas, prop, P=1, T=298.15):
-    """For a gas,"""
-    P_Pa = P * 1e5 # bar to Pa
-    if prop == 'Vmolar':
-        return 1000/CP.PropsSI('Dmolar', 'P', P_Pa, 'T', T, gas) # L/mol
+def getprop(gas, prop, P=None, T=None, **kwargs):
+    """For a gas, get a thermodynamic property at a given pressure and temperature.
+    
+    Parameters
+    ----------
+    gas: string
+        the gas name (e.g. 'Air', 'Ar', 'CO2')
+    prop: string
+        the thermodynamic property ('Smolar', 'Umolar', 'Vmolar', etc)
+    P: float
+        the pressure in bar
+    T: float
+        the temperature in K
+
+    Returns
+    -------
+    float
+        The value of the property under the given conditions.
+    """
+    if len(kwargs) == 0:
+        if P is None:
+            P = 1
+        if T is None:
+            T = 298.15
+        P_Pa = P * 1e5 # bar to Pa
+        if prop == 'Vmolar':
+            return 1000/CP.PropsSI('Dmolar', 'P', P_Pa, 'T', T, gas) # L/mol
+        else:
+            return CP.PropsSI(prop, 'P', P_Pa, 'T', T, gas)
     else:
-        return CP.PropsSI(prop, 'P', P_Pa, 'T', T, gas)
+        args = []
+        if P is not None:
+            args.append('P')
+            args.append(P*1e5)
+        if T is not None:
+            args.append('T')
+            args.append(T)
+        for key, val in kwargs.items():
+            args.append(key)
+            args.append(val)
+        
+        args.append(gas)
+        
+        if prop == 'Vmolar':
+            return 1000/CP.PropsSI('Dmolar', *args) # L/mol
+        else:
+            return CP.PropsSI(prop, *args)
+
+
 
 def getprop_dens(gas, prop, density, T=298.15):
     """For a gas,"""
@@ -91,11 +133,12 @@ def getprop_dens(gas, prop, density, T=298.15):
 
 
     
-def getPressure(gas, T=300, Vbar=22.4):
+def getPressure(gas, T=300, Vbar=22.4, threshold=1e-4):
+    """Get the pressure of a gas at a given temperature and molar volume."""
     Z_prev = 1
     Pguess = (0.083145*T)/Vbar * Z_prev
     Z_guess = getprop(gas, 'Z', Pguess, T)
-    while abs(Z_guess-Z_prev) > 0.001:
+    while abs(Z_guess-Z_prev) > threshold:
         Z_prev=Z_guess
         Pguess = Pguess*Z_guess
         Z_guess = getprop(gas, 'Z', Pguess, T)
@@ -107,7 +150,26 @@ def getPressure(gas, T=300, Vbar=22.4):
 def _flatten(a):
     return functools.reduce(operator.iconcat, a, [])
 
+
 def getprop_df(gas, prop, P, T):
+    """Get a gas property as a dataframe.
+    
+    Parameters
+    ----------
+    gas: string
+        the gas name (e.g. 'Air', 'Ar', 'CO2')
+    prop: string
+        the thermodynamic property ('Smolar', 'Umolar', 'Vmolar', etc)
+    P: array-like
+        the pressure in bar
+    T: array-like
+        the temperature in K
+    
+    Returns
+    -------
+    pandas.DataFrame
+        The dataframe with the given property initial conditions and properties.
+    """
     P = np.array(P).reshape(-1)
     T = np.array(T).reshape(-1)
     
@@ -119,8 +181,26 @@ def getprop_df(gas, prop, P, T):
     return df
 
 def getprops_df(gas, props, P, T):
-    P = np.array(P).reshape(-1)
-    T = np.array(T).reshape(-1)
+    """Get a gas property as a dataframe.
+    
+    Parameters
+    ----------
+    gas: string
+        the gas name (e.g. 'Air', 'Ar', 'CO2')
+    props: array-like
+        a list of properties to return, e.g. ['Smolar', 'Umolar', 'Vmolar']
+    P: array-like
+        the pressure in bar
+    T: array-like
+        the temperature in K
+    
+    Returns
+    -------
+    pandas.DataFrame
+        The dataframe with the given property initial conditions and properties.
+    """
+    P = np.array(P).reshape(-1) # Make sure it's a 1D array
+    T = np.array(T).reshape(-1) # Make sure it's a 1D array
     dicts = []
     for Px in P:
         for Tx in T:
