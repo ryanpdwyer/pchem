cb00c6412d8b9a607042655c34a6581ec3928731
Ryan Dwyer <ryanpdwyer@gmail.com>
2024-09-16 01:46:45 -0400

    Avoid clobbering iamgemodel parameters

--- apps/imagemodel_regression.py
+++ apps/imagemodel_regression.py
@@ -66,7 +66,7 @@ def run():
     st.write("Use the system prompt below to try to build a zero-shot regression model that places a circle over the image at a given point. Right now, it tries to find the position of the person's nose. Try modifying the prompt to do something else too.")
 
     if 'system' not in st.session_state:
-        st.session_state.system = """Respond to an input image with the relative position of the tip of the person in the image's nose. Use the format (x, y) for your output, where x = 0 is the image's left edge, x = 100 is the image's right edge, y = 0 is the image's top edge, and y = 100 is the image's bottom edge. If there is no nose in the image, respond no nose. Do not give any other response."""
+        st.session_state.system_reg = """Respond to an input image with the relative position of the tip of the person in the image's nose. Use the format (x, y) for your output, where x = 0 is the image's left edge, x = 100 is the image's right edge, y = 0 is the image's top edge, and y = 100 is the image's bottom edge. If there is no nose in the image, respond no nose. Do not give any other response."""
     
     if 'cost' not in st.session_state:
         st.session_state.cost = 0.0
@@ -74,7 +74,7 @@ def run():
     if 'model' not in st.session_state:
         st.session_state.model = "gpt-4o-mini"
 
-    system = st.text_area("System prompt", st.session_state.system)
+    system = st.text_area("System prompt", st.session_state.system_reg)
 
     model = st.selectbox("Model", ["gpt-4o-mini", "gpt-4o"], index=0)
 
@@ -106,18 +106,18 @@ def run():
             draw.ellipse([coords[0]*x_dim/100.0-10, coords[1]*y_dim/100.0-10, coords[0]*x_dim/100.0+10, coords[1]*y_dim/100.0+10], outline='red', width=2)
             st.image(img)
 
-        if system != st.session_state.system:
-            st.session_state.system = system
+        if system != st.session_state.system_reg:
+            st.session_state.system_reg = system
         
         if model != st.session_state.model:
             st.session_state.model = model
 
 
 
-    if submit and picture is not None and (system != st.session_state.system or model != st.session_state.model):
+    if submit and picture is not None and (system != st.session_state.system_reg or model != st.session_state.model):
         response = get_response(system, img, model)
 
-        st.session_state.system = system
+        st.session_state.system_reg = system
         st.session_state.model = model
 
 
